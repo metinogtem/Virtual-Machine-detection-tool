ea99d3973bf7375026e6dc382b08d00ba9ac7374
metinogtem <metinogtem@gmail.com>
2022-07-23 19:14:33 +0100

    Create service_check.cs

---
+++ Virtual Machine detection tool/service_check.cs
@@ -0,0 +1,39 @@
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+public class VMwareDetect
+{
+    
+    public async Task<bool> isVMware(){
+    
+        string[] KNOWN_SERVICES = { "VMTools", "Vmhgfs", "vm3dservice", "Vmmouse", "Vmrawdsk", "Vmusbmouse", "Vmvss", "Vmscsi", "Vmxnet", "vmx_svga", "Vmware Tools", "Vmware Physical Disk Helper Service" };
+        
+        List<string>? serviceOutput = await Utils.RunPowershellCommand("get-service", null, "Name");
+            if (serviceOutput != null)
+            {
+                foreach(string service in KNOWN_SERVICES)
+                {
+                    foreach (string s in serviceOutput)
+                    {
+                        if (string.Compare(s.ToLower(), service.ToLower()) == 0)
+                            return true;
+                    }
+                }
+                
+          return false;
+            
+        }
+
+    
+    public static void Main(string[] args)
+    {
+        
+        isVMware();
+    }
+}
